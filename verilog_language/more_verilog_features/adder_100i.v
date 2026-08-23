3efbf01823bb86d334784172c507c4f0697ce390
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-08-23 15:25:38 +0530

    Create adder_100i.v

---
+++ verilog_language/more_verilog_features/adder_100i.v
@@ -0,0 +1,28 @@
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+    
+    fadd f0(a[0],b[0],cin,sum[0],cout[0]);
+    
+    genvar i; 
+    generate 
+        for (i=1;i<100;i=i+1)
+            begin:rca_loop
+                fadd f(a[i],b[i],cout[i-1],sum[i],cout[i]);
+            end
+    endgenerate
+    
+
+endmodule
+
+module fadd(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout);
+    assign sum=a^b^cin;
+    assign cout= a&b | b&cin | cin&a;
+endmodule
