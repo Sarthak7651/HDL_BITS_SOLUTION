991af70cb726e2bd62428d8fd4b807a60d74c3a9
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-08-23 15:36:22 +0530

    Create bcdadd100.v

---
+++ verilog_language/more_verilog_features/bcdadd100.v
@@ -0,0 +1,26 @@
+module top_module (
+    input  [399:0] a,
+    input  [399:0] b,
+    input          cin,
+    output         cout,
+    output [399:0] sum
+);
+
+    wire [100:0] carry;
+    assign carry[0] = cin;
+    assign cout = carry[100];
+
+    genvar i;
+    generate
+        for (i = 0; i < 100; i = i + 1) begin : bcd
+            bcd_fadd fa (
+                .a(a[4*i+3:4*i]),
+                .b(b[4*i+3:4*i]),
+                .cin(carry[i]),
+                .cout(carry[i+1]),
+                .sum(sum[4*i+3:4*i])
+            );
+        end
+    endgenerate
+
+endmodule
