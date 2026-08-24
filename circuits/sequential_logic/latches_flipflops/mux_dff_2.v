2c7416ea97b0f71a55fa42a38f68bc15abfa1ebe
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-08-24 22:05:22 +0530

    Create mux_dff_2.v

---
+++ circuits/sequential_logic/latches_flipflops/mux_dff_2.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+    wire t1,t2;
+    
+    assign t1 = E ? w : Q;
+    assign t2 = L ? R : t1;
+    
+    always @(posedge clk)
+        begin
+            Q <= t2;
+        end
+
+endmodule
