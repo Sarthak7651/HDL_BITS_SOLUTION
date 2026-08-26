75b4a3b0ed27bdccafc38ace312788a192c5dc67
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-08-26 10:49:04 +0530

    Create simple_fsm2_asynrst.v

---
+++ circuits/sequential_logic/finite_state_machine/simple_fsm2_asynrst.v
@@ -0,0 +1,36 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(posedge clk or posedge areset)
+        begin
+            if (areset)
+                state <= OFF;
+            else state <= next_state;
+        end
+    
+    always @(*)
+        begin
+            case (state)
+                OFF : begin
+                    if (j)
+                        next_state = ON;
+                    else next_state = OFF;
+                    out = 1'b0;
+                end
+                        
+                ON : begin
+                    if (k)
+                        next_state = OFF;
+                    else next_state = ON;
+                    out = 1'b1;
+                end
+            endcase
+        end
+endmodule
